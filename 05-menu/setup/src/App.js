27799986c64ab6649bb0d739f4f663dc207c9f21
Shir Fogel <shirfogel@cs.colman.ac.il>
2021-12-19 16:20:56 +0200

    doing the menu project with dynamic changes

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,8 +3,41 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+//we want to get all the name of the categories
+//we use set in order to get only one time the specific category
+//we want to get it in array
+const allCategories=['all',...new Set(items.map((item)=>item.category))];
+console.log(allCategories);
+
 function App() {
-  return <h2>menu project setup</h2>;
+  //here we get the json from the data and we send it as propery in the component below
+  const[menuItems,setMenuItems]=useState(items);
+  const[categories,setCategories]=useState(allCategories);
+
+  //we filter the original list with the category
+  const filterItems=(category)=>{
+    //we want to display the all list
+    if(category==='all'){
+      setMenuItems(items);
+      return;
+    }
+    //we will work with the original list
+    const newItems=items.filter((item)=>item.category===category);
+    setMenuItems(newItems);
+  };
+
+  return (
+  <main>
+   <section className='menu selection'>
+     <div className='title'>
+       <h2>our menu</h2>
+       <div className='underline'></div>
+     </div>
+     <Categories categories={categories} filterItems={filterItems}/>
+     <Menu items={menuItems}/>
+   </section>
+    </main>
+  );
 }
 
 export default App;
